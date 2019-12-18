95f215a3a7a69329b5e8828b006cc9bc6286522b
CrispyBacon1999 <bacon.josh09@gmail.com>
2019-12-17 19:31:52 -0500

    Update version number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 setup(
     name="RobotPy-Limelight",
-    version=os.environ["TRAVIS_TAG"],
+    version="2020.0.3",
     author="Josh Bacon",
     author_email="bacon.josh09@gmail.com",
     packages=["limelight"],
